356a940cd746d7710ef0fa93ff2cfdd8561b71af
Tyler Lemburg <trlemburg@gmail.com>
2016-09-05 11:47:22 -0500

    unicorn file

---
+++ unicorn.rb
@@ -0,0 +1,6 @@
+worker_processes 1
+working_directory '/home/tyler/simbasparents.com'
+pid '/var/run/simbasparents.pid'
+
+stderr_path '/var/log/simbasparents_err.log'
+stdout_path '/var/log/simbasparents_out.log'
