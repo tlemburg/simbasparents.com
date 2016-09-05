c6b10b5c0bb2c620184124e370568a0ed3c92377
Tyler Lemburg <tyler@huskerbase.com>
2016-09-05 11:49:39 -0500

    on-server fixes

--- unicorn.rb
+++ unicorn.rb
@@ -1,6 +1,6 @@
 worker_processes 1
 working_directory '/home/tyler/simbasparents.com'
-pid '/var/run/simbasparents.pid'
+pid '/var/run/simbasparents/simbasparents.pid'
 
-stderr_path '/var/log/simbasparents_err.log'
-stdout_path '/var/log/simbasparents_out.log'
+stderr_path '/var/log/simbasparents/simbasparents_err.log'
+stdout_path '/var/log/simbasparents/simbasparents_out.log'
