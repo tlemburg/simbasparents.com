619f5ad28ddaf7090c2ac92174539436d9d99e60
Tyler Lemburg <trlemburg@gmail.com>
2016-11-22 21:00:20 -0600

    commit

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
 get '/' do
-  erb 'Tyler and Abby wedding', :layout => :layout
+  erb :home, :layout => :layout
+end
+
+get '/location/?' do
+  erb :location, :layout => :layout
+end
+
+get '/rsvp/?' do
+  erb :rsvp, :layout => :layout
+end
+
+get '/charities/?' do
+  erb :registry, :layout => :layout
 end
