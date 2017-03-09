7f8e417bbedc9b542d63630ad095d5eefa90dc3a
Tyler Lemburg <trlemburg@gmail.com>
2017-03-09 11:48:32 -0600

    final

--- app.rb
+++ app.rb
@@ -1,19 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  redirect '/abby-neyer-and-tyler-lemburg
+  redirect '/abby-neyer-and-tyler-lemburg'
 end
 
 get '/abby-neyer-and-tyler-lemburg/?' do
   erb :home, :layout => :layout
-end
-
-get '/location/?' do
-  erb :location, :layout => :layout
-end
-
-get '/rsvp/?' do
-  erb :rsvp, :layout => :layout
 end
 
 get '/charities/?' do
